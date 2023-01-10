b74643fe5a373e65dee1220be8bb83cafd3aa806
Guttorm R. T. Zapffe <96119736+Mrottug@users.noreply.github.com>
2023-01-10 12:35:17 +0100

    Add files via upload

---
+++ SQL TEST/import sqlite3 as sq.py
@@ -0,0 +1,27 @@
+import sqlite3 as sq
+
+import sys
+
+
+
+def main():
+
+    conn = sq.connect('personer.db')
+
+    c = conn.cursor()
+
+    c.execute('CREATE TABLE IF NOT EXISTS name (Fornavn TEXT, Etternavn TEXT, Epost TEXT, Telefon TEXT, Postnummer TEXT)')
+
+    f = open("randoms.csv", "r")
+
+    for line in f:
+
+        c.execute('INSERT INTO name VALUES (?,?,?,?,?)', (line.split(',')))
+
+    conn.commit()
+
+    c.execute('SELECT * FROM name')
+
+    conn.close()
+
+main()
